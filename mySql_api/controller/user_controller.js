8e61679ad7446fd7854d9357ea86df046bbd1b8c
Carlos <carlitosviano@gmail.com>
2023-01-16 19:11:09 +0100

    dndm

--- mySql_api/controller/user_controller.js
+++ mySql_api/controller/user_controller.js
@@ -2,93 +2,92 @@ import { jwtVerify, SignJWT } from "jose";
 import md5 from "md5";
 import dao from "../services/dao.js";
 
-
 const controller = {};
 
-
 controller.addUser = async (req, res) => {
-    const {name,email,password} = req.body;
+  const { name, email, password } = req.body;
 
-    if(!name ||!email || !password) return res.status(400).send("Error al recibir el body")
+  if (!name || !email || !password)
+    return res.status(400).send("Error al recibir el body");
 
-    try{
-        const user = await dao.getUserByEmail(email)
+  try {
+    const user = await dao.getUserByEmail(email);
 
-        if(user.length > 0) return res.status(409).send("usuario ya registrado");
-        
-        const addUser = await dao.addUser(req.body)
-        if(addUser) return res.send(`Usuario ${name} con id: ${addUser} registrado`)
-    } catch(e){
-        console.log(e.message)
-    }
+    if (user.length > 0) return res.status(409).send("usuario ya registrado");
 
-}
+    const addUser = await dao.addUser(req.body);
+    if (addUser)
+      return res.send(`Usuario ${name} con id: ${addUser} registrado`);
+  } catch (e) {
+    console.log(e.message);
+  }
+};
 
-controller.loginUser = async (req,res) =>{
+controller.loginUser = async (req, res) => {
+  const { email, password } = req.body;
 
-    const {email, password} = req.body;
+  if (!email || !password)
+    return res.status(400).send("Error al recibir el body");
 
-    if(!email || !password)
-    return res.status(400).send("Error al recibir el body")
+  try {
+    let user = await dao.getUserByEmail(email);
+    if (user.length <= 0) return res.status(404).send("usuario no registrado");
+    const clientPassword = md5(password);
+    console.log(user);
 
-    try{
-   
-        let user = await dao.getUserByEmail(email)
-        if (user.length <= 0) return res.status(404).send("usuario no registrado")
-        const clientPassword = md5(password)
-        console.log(user)
-        
+    // [user] = user;
+    const [newUser] = user;
 
-       // [user] = user;
-       const [newUser] = user
+    if (newUser.password !== clientPassword)
+      return res.status(401).send("Password incorrecta");
 
-        if(newUser.password !== clientPassword)
-        return res.status(401).send("Password incorrecta")
+    const jwtConstructor = new SignJWT({
+      id: newUser.id,
+      email,
+      role: newUser.role,
+    });
 
-        const jwtConstructor = new SignJWT({
-            id: newUser.id,
-            email,
-            role: newUser.role,
-        })
-
-        const encoder = new TextEncoder();
-
-        const jwt = await jwtConstructor
-        .setProtectedHeader({alg: "HS256", typ: "JWT"})
-        .setIssuedAt()
-        .setExpirationTime("1h")
-        .sign(encoder.encode(process.env.JWT_SECRET))
-
-        return res.send({jwt})
-    } catch(e){
-        console.log(e.message)
-    }
-}
-
-controller.deleteUser = async (req,res) => {
-
-const {authorization} = req.headers;
-
-if(!authorization) return res.sendStatus(401)
-const token = authorization.split(" ")[1];
-
-try {
     const encoder = new TextEncoder();
 
-    const {payload} = await jwtVerify(token, encoder.encode(process.env.JWT_SECRET))
+    const jwt = await jwtConstructor
+      .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+      .setIssuedAt()
+      .setExpirationTime("1h")
+      .sign(encoder.encode(process.env.JWT_SECRET));
 
-    if(!payload.role) return res.status(409).send("no tiene permiso de administrador");
+    return res.send({ jwt });
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+controller.deleteUser = async (req, res) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) return res.sendStatus(401);
+  const token = authorization.split(" ")[1];
+
+  try {
+    const encoder = new TextEncoder();
+
+    const { payload } = await jwtVerify(
+      token,
+      encoder.encode(process.env.JWT_SECRET)
+    );
+
+    if (!payload.role)
+      return res.status(409).send("no tiene permiso de administrador");
 
     const user = await dao.getUserById(req.params.id);
 
-    if(user.length <= 0) return res.status(404).send("el usuario no existe");
+    if (user.length <= 0) return res.status(404).send("el usuario no existe");
 
     await dao.deleteUser(req.params.id);
 
-    return res.send(`El usuario con id ${req.params.id} eliminado`)
-} catch(e){
-    console.log(e.message)
-}
-}
+    return res.send(`El usuario con id ${req.params.id} eliminado`);
+  } catch (e) {
+    console.log(e.message);
+  }
+};
 
-export default controller
+export default controller;
